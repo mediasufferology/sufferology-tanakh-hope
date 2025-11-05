8f62eccae09859e48d198cd3122f37e275c7df92
mediasufferology <media.sufferology@gmail.com>
2025-11-05 00:20:35 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,6 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
 
-      export default defineConfig({
-  base: "/",
-});
-
         
     },
   },
